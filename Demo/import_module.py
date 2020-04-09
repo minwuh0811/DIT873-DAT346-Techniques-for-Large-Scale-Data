04d6b321801ab749c764b977e5b8c9a909d28ae3
minwuh081@gmail.com <minwuh081@gmail.com>
2020-04-09 09:39:15 +0200

    __name__ ==__main__ example

---
+++ Demo/import_module.py
@@ -0,0 +1,3 @@
+import module_name
+
+print ("Second Module's Name: {}".format(__name__))
---
+++ Demo/module_name.py
@@ -0,0 +1,6 @@
+def main():
+    print ("First Module's Name: {}".format(__name__))
+if __name__  =='__main__':
+    main()
+else:
+    print ("from inport")
